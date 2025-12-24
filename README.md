b500069d6abc630c1c6d60cebfe9e6de8de174a2
Josep Maria Sanz Xicola <jsanz83@gmail.com>
2025-12-24 23:48:41 +0100

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+<p align="center">
+  <img src="static/falk.webp" alt="falk" width="350"/>
+</p>
+
+# Flak
+
+
--- src/falk/__init__.py
+++ src/falk/__init__.py
@@ -1,2 +1,3 @@
-def hello() -> str:
-    return "Hello from falk!"
+from ._version import __version__
+
+__all__ = ['__version__']
